b0d6a939cfcea7684762c3c0b1543517a79208d2
Pablo Martins <pablomartins1515@hotmail.com>
2022-12-22 20:07:27 -0300

    Update Termos.tsx
    Atualização dos termos para PERSONAL PERFETO

--- src/componentes/Termos.tsx
+++ src/componentes/Termos.tsx
@@ -14,85 +14,78 @@ export function Termos () {
 
             <div >
                 <h1 className="m-2 p-2 flex justify-center font-black text-4xl">
-                    Privacy Policy
+                   Aviso de Privacidade do aplicativo "PERSONAL PERFEITO"
                 </h1>
 
                 <h3 className="flex text-left p-8">
-                    buscardados.com.br website is owned by buscardados, which is a data controller of your personal data. We have adopted this Privacy Policy, which determines how we are processing the information collected by buscardados.com.br, which also provides the reasons why we must collect certain personal data about you. Therefore, you must read this Privacy Policy before using buscardados.com.br website.
+                    1. Quais informações estão presentes neste documento?
+                    Neste Termo de Uso, o usuário do serviço Aplicativo do PERSONAL PERFEITO, encontrará informações sobre: o funcionamento do serviço e as regras aplicáveis a ele; o arcabouço legal relacionado à prestação do serviço; as responsabilidades do usuário ao utilizar o serviço; as responsabilidades da administração pública ao prover o serviço; informações para contato, caso exista alguma dúvida ou seja necessário atualizar informações; e o foro responsável por eventuais reclamações caso questões deste Termo de Uso tenham sido violadas.
+
+                    2. Aceitação do Termo de Uso e Política de Privacidade 
+                    Ao utilizar os serviços, o usuário confirma que leu e compreendeu os Termos e Políticas aplicáveis ao serviço Aplicativo PERSONAL PERFEITO e concorda em ficar vinculado a eles.
+
+                    3. Definições
+                    Para melhor compreensão deste documento, neste Termo de Uso e Política de Privacidade, consideram-se:
+                    Dado pessoal: informação relacionada a pessoa natural identificada ou identificável.
+                    Titular: pessoa natural a quem se referem os dados pessoais que são objeto de tratamento.
+                    Controlador: pessoa natural ou jurídica, de direito público ou privado, a quem competem as decisões referentes ao tratamento de dados pessoais.
+                    Agentes de tratamento: o controlador.
+                    Controlador: pessoa natural ou jurídica, de direito público ou privado, que realiza o tratamento de dados pessoais em nome do controlador.
+                    Tratamento: toda operação realizada com dados pessoais, como as que se referem a coleta, produção, recepção, classificação, utilização, acesso, reprodução, transmissão, distribuição, processamento, arquivamento, armazenamento, eliminação, avaliação ou controle da informação, modificação, comunicação, transferência, difusão ou extração.
+                    Uso compartilhado de dados: comunicação, difusão, transferência internacional, interconexão de dados pessoais ou tratamento compartilhado de bancos de dados pessoais por órgãos e entidades públicos no cumprimento de suas competências legais, ou entre esses e entes privados, reciprocamente, com autorização específica, para uma ou mais modalidades de tratamento permitidas por esses entes públicos, ou entes privados.
+                    Sítios (sites) e aplicativos: sítios e aplicativos por meio dos quais o usuário acessa os serviços e conteúdos disponibilizados.
+                    Terceiro: Pessoa ou entidade que não participa diretamente em um contrato, em um ato jurídico ou em um negócio, ou que, para além das partes envolvidas, pode ter interesse num processo jurídico.
+                    Usuários (ou "Usuário", quando individualmente considerado): todas as pessoas naturais que utilizarem o serviço Aplicativo e site do PERSONAL PERFEITO.
+
+                    4. Descrição do serviço
+
+
+
+                    5. Agentes de tratamento
+                    5.1. A quem compete as decisões referentes ao tratamento de dados pessoais realizado no serviço Aplicativo PERSONAL PERFEITO (Controlador)?
+                    Para o serviço Aplicativo PERSONAL PERFEITO, as decisões referentes ao tratamento de dados pessoais são de responsabilidade da EMPRESA I9Ativa S.A. Endereço: Rua José do Egito n° 82  - São Raimundo das Mangabeiras - MA - CEP 65.840-000. Telefone: +55 (99) 9 84574733.
+                    6. Quais são as leis e normativos aplicáveis a esse Termo?
+                    Lei nº 12.965, de 23 de abril de 2014 - Marco Civil da Internet – Estabelece princípios, garantias, direitos e deveres para o uso da Internet no Brasil.
+                    Lei nº 12.527, de 18 de novembro de 2011 - Lei de Acesso à Informação – Regula o acesso a informações previsto na Constituição Federal.
+                    Lei nº 13.460, de 26 de junho de 2017 - Dispõe sobre participação, proteção e defesa dos direitos do usuário dos serviços públicos da administração pública.
+                    Lei nº 13.709, de 14 de agosto de 2018 - Dispõe sobre o tratamento de dados pessoais, inclusive nos meios digitais, por pessoa natural ou por pessoa jurídica de direito público ou privado, com o objetivo de proteger os direitos fundamentais de liberdade e de privacidade e o livre desenvolvimento da personalidade da pessoa natural.
+                    Decreto nº 8.777, de 11 de maio de 2016 - Institui a Política de Dados Abertos do Poder Executivo federal.
+                    Decreto nº 7.724, de 16 de maio de 2012 - Regulamenta a Lei no 12.527, de 18 de novembro de 2011 (Lei de Acesso à Informação), que dispõe sobre o acesso a informações previsto na Constituição Decreto nº 10.046, de 09 de outubro de 2019 - Dispõe sobre a governança no compartilhamento de dados no âmbito da administração pública federal e institui o Cadastro Base do Cidadão e o Comitê Central de Governança de Dados.
+                    8. Quais são os direitos do usuário do serviço? 
+                    O usuário do serviço possui os seguintes direitos, conferidos pela Lei de Proteção de Dados Pessoais:
+                    - Direito de confirmação e acesso (Art. 18, I e II): é o direito do usuário de obter do serviço a confirmação de que os dados pessoais que lhe digam respeito são ou não objeto de tratamento e, se for esse o caso, o direito de acessar os seus dados pessoais.
+                    - Direito de retificação (Art. 18, III): é o direito de solicitar a correção de dados incompletos, inexatos ou desatualizados.
+                    - Direito à limitação do tratamento dos dados (Art. 18, IV): é o direito do usuário de limitar o tratamento de seus dados pessoais, podendo exigir a eliminação de dados desnecessários, excessivos ou tratados em desconformidade com o disposto na Lei Geral de Proteção de Dados.
+                    - Direito de oposição (Art. 18, § 2º): é o direito do usuário de, a qualquer momento, se opor ao tratamento de dados por motivos relacionados com a sua situação particular, com fundamento em uma das hipóteses de dispensa de consentimento ou em caso de descumprimento ao disposto na Lei Geral de Proteção de Dados.
+                    - Direito de não ser submetido a decisões automatizadas (Art. 20, LGPD): o titular dos dados tem direito a solicitar a revisão de decisões tomadas unicamente com base em tratamento automatizado de dados pessoais que afetem seus interesses, incluídas as decisões destinadas a definir o seu perfil pessoal, profissional, de consumo e de crédito ou os aspectos de sua personalidade.
+                    9. Quais são as obrigações dos usuários que utilizam o serviço? 
+                    O login e senha só poderão ser utilizados pelo usuário cadastrado. Ele se compromete em manter o sigilo da senha, que é pessoal e intransferível, não sendo possível, em qualquer hipótese, a alegação de uso indevido, após o ato de compartilhamento.
+                    O Usuário é responsável pela reparação de todos e quaisquer danos, diretos ou indiretos (inclusive decorrentes de violação de quaisquer direitos de outros usuários, de terceiros, inclusive direitos de propriedade intelectual, de sigilo e de personalidade), que sejam causados à Administração Pública, a qualquer outro Usuário, ou, ainda, a qualquer terceiro, inclusive em virtude do descumprimento do disposto nestes Termos de Uso e Política de Privacidade ou de qualquer ato praticado a partir de seu acesso ao serviço.
+                    O Órgão não poderá ser responsabilizado pelos seguintes fatos:
+                    a. Equipamento infectado ou invadido por atacantes;
+                    b. Equipamento avariado no momento do consumo de serviços;
+                    c. Proteção do computador;
+                    d. Proteção das informações baseadas nos computadores dos usuários;
+                    e. Abuso de uso dos computadores dos usuários;
+                    f. Monitoração clandestina do computador dos usuários;
+                    g. Vulnerabilidades ou instabilidades existentes nos sistemas dos usuários;
+                    h. Perímetro inseguro;
+
+                    SOBRE A POLÍTICA DE PRIVACIDADE
+                    Esta Política de Privacidade foi elaborada em conformidade com todas as leis que abrem o assunto supracitadas acima, e nela, o usuário do serviço Aplicativo e site do PERSONAL PERFEITO encontrará informações sobre: qual o tratamento dos dados pessoais realizados, de forma automatizada ou não, e a sua finalidade; os dados pessoais dos usuários necessários para a prestação do serviço; a forma como eles são coletados; se há o compartilhamento de dados com terceiros; e quais as medidas de segurança implementadas para proteger os dados.
+
+                    TERMO DE AUTORIZAÇÃO PARA ENVIO DE MENSAGENS (VIA CELULAR E E-MAIL)
+                    Com a concordância deste termo, declaro que o telefone e e-mail informados são de minha propriedade e autorizo o Ministério da Cidadania a enviar mensagens por esses canais utilizando os dados e autorizações informados por mim no Aplicativo PERSONAL PERFEITO, de acordo com as seguintes condições:
+                    a. o envio de mensagens se dará somente para fins de implementação de políticas públicas e realização de estudos e pesquisas, conforme previsto no Decreto nº 11.016, de 29 de março de 2022, que regulamenta o Cadastro Único, e JAMAIS serão utilizadas para fins comerciais;
+                    b. as mensagens sempre conterão descrição clara do remetente e darão a opção de se remover imediatamente do recebimento de novas mensagens;
+                    c. as mensagens se limitarão a textos e JAMAIS conterão links, endereços de e-mail, propagandas de terceiros, arquivos anexos, solicitação de senha nem pedidos de autorização;
+                    d. o serviço de envio de mensagens será prestado por prazo indeterminado, podendo ser cancelado a qualquer tempo sem aviso prévio, sem prejuízo para minha pessoa ou ainda para a Empresa I9Ativa S.A. a e os seus operadores;
+                    e. a I9Ativa S.A. e os seus operadores não se responsabilizam por problemas nos serviços de celular e e-mail nem por acessos de outras pessoas às mensagens recebidas por mim;
+                    f. as dúvidas sobre o envio de mensagens podem ser esclarecidas pela Central de Atendimento da I9Ativa S.A. , pelo telefone +5599984574733 ou por meio eletrônico: pablomartins1515@gmail.com
+
                  </h3>
-                 <h3 className="flex text-left p-8">
-                     We take care of your personal data and undertake to guarantee its confidentiality and security.
-                 </h3>
 
-                 <h2 className="p-8 font-black text-lx2">
-                 Why do we process your data?
-                 </h2>
-
-                 <h3 className="flex text-left p-8">
-                 Our top priority is customer data security, and, as such, we may process only minimal user data, only as much as it is absolutely necessary to maintain the website. Information collected automatically is used only to identify potential cases of abuse and establish statistical information regarding website usage. This statistical information is not otherwise aggregated in such a way that it would identify any particular user of the system.
-                 </h3>
-
-                 <h3 className="flex text-left p-8">
-                You can visit the website without telling us who you are or revealing any information, by which someone could identify you as a specific, identifiable individual. If, however, you wish to use some of the website’s features, or you wish to receive our newsletter or provide other details by filling a form, you may provide personal data to us, such as your email, first name, last name, city of residence, organization, telephone number. You can choose not to provide us with your personal data, but then you may not be able to take advantage of some of the website’s features. For example, you won’t be able to receive our Newsletter or contact us directly from the website. Users who are uncertain about what information is mandatory are welcome to contact us via pablomartins1515@gmail.com
-                 </h3>
-
-                 <h2 className="p-8 font-black text-lx2">
-                    Your rights:
-
-                 </h2>
-
-                 <h3 className="flex text-left p-8">
-                    If you are a European resident, you have the following rights related to your personal data: 
-                </h3>
-
-                <ul className="p-12">
-                    <li>• The right to be informed.</li>
-                    <li>• The right of access.</li>
-                    <li>• The right to rectification.</li>
-                    <li>• The right to erasure.</li>
-                    <li>• The right to restrict processing.</li>
-                    <li>• The right to data portability.</li>
-                    <li>• The right to object.</li>
-                    <li>• Rights in relation to automated decision-making and profiling.</li>              
-                </ul>
-
-                <h3 className="flex text-left p-8">
-                If you would like to exercise this right, please contact us through the contact information below.
-                Additionally, if you are a European resident, we note that we are processing your information in order to fulfill contracts we might have with you (for example, if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above. Additionally, please note that your information might be transferred outside of Europe, including Canada and the United States.
-                 </h3>
-
-                 <h2 className="p-8 font-black text-lx2">
-                 Links to other websites:
-                 </h2>
-
-                 <h3 className="flex text-left p-8">
-                 Our website may contain links to other websites that are not owned or controlled by us. Please be aware that we are not responsible for such other websites or third parties' privacy practices. We encourage you to be aware when you leave our website and read the privacy statements of each website that may collect personal information.
-                </h3>
-
-                <h2 className="p-8 font-black text-lx2">
-                    Information security:
-
-                 </h2>
-
-                 <h3 className="flex text-left p-8">
-                 We secure information you provide on computer servers in a controlled, secure environment, protected from unauthorized access, use, or disclosure. We keep reasonable administrative, technical, and physical safeguards to protect against unauthorized access, use, modification, and personal data disclosure in its control and custody. However, no data transmission over the Internet or wireless network can be guaranteed.
-                </h3>
-
-                <h2 className="p-8 font-black text-lx2">
-                Legal disclosure:
-                 </h2>
-
-                 <h3 className="flex text-left p-8">
-                 We will disclose any information we collect, use or receive if required or permitted by law, such as to comply with a subpoena or similar legal process, and when we believe in good faith that disclosure is necessary to protect our rights, protect your safety or the safety of others, investigate fraud, or respond to a government request.
-                </h3>
-
-                <h2 className="p-8 font-black text-lx2">
-                Contact information:
-                 </h2>
-
-                 <h3 className="flex text-left p-8">
-                 If you would like to contact us to understand more about this Policy or wish to contact us concerning any matter relating to individual rights and your Personal Information, you may send an email to pablomartins1515@gmail.com.
-                </h3>
+                
 
             </div>
     </div>
